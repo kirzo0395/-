63f8ec171c67bc26ea02296e357fb4798fd4f9fc
Анатолий <92919229+kirzo0395@users.noreply.github.com>
2024-02-19 20:48:13 +0300

    Add files via upload
    Сведения о студенте

---
+++ main.swift
@@ -0,0 +1,12 @@
+let group = "0395"
+let weight = 70
+let firstName = "Анатолий"
+let secondName = "Стручин"
+let univercity = "Санкт-Петербургский государственный электротехнический университет"
+let age = 21
+let height = 187
+let faculty = "ФКТИ"
+let direction = "Системный анализ и управление"
+print("СВОДНАЯ ИНФОРМАЦИЯ О СТУДЕНТЕ")
+print("Имя: \(firstName) \nФамилия: \(secondName) \nВозраст: \(age) \nРост: \(height) \nВес: \(weight)")
+print("Место учебы: \(univercity) \nФакультет: \(faculty)\nНаправление: \(direction) \nГруппа: \(group)")
